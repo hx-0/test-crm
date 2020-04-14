7a1bd41460088e43bb7e524b629673204ed299d6
“haopeng <>
2020-04-14 15:40:46 +0800

    添加Test02

--- src/main/java/com/hx/test/Test02.java
+++ src/main/java/com/hx/test/Test02.java
@@ -1,9 +1,8 @@
 package com.hx.test;
 
-public class Test {
+public class Test02 {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("rhp");
     }
-
 }
